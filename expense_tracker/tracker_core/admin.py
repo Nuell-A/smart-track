3d08a7823171dc2ed94d1d40937c97667005004d
Nuell <alcala.arredondo1@gmail.com>
2024-06-21 13:51:52 -0700

    Added models, registered them to Admin panel and implemented default login page.

--- expense_tracker/tracker_core/admin.py
+++ expense_tracker/tracker_core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Budget
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Budget)
+class CategoryExpense(admin.ModelAdmin):
+    list_display = ('category', 'amount',)
+    search_fields = ('category',)
--- expense_tracker/tracker_core/models.py
+++ expense_tracker/tracker_core/models.py
@@ -1,6 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
 class Expense(models.Model):
     # Create fields for name, amount, category, and date. 
-    pass
+    name = models.CharField(max_length=150)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"Expense:\n{self.amount} - {self.name} - {self.category} - {self.date}"
+
+class Income(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"Income:\n{self.amount} - {self.name} - {self.date}"
+
+class Budget(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"Budget for {self.category}: {self.amount}"
