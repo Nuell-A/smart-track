217e235fd0c1f81e4a1781c4eb93ff698a654ead
Nuell-LTL <alcala.arredondo1@gmail.com>
2024-06-22 00:01:18 -0700

    Created forms.py for user registration page. Tested with test accounts

--- expense_tracker/tracker_core/admin.py
+++ expense_tracker/tracker_core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Budget
+from .models import Category, Budget, Expense, Income
 
 # Register your models here.
 @admin.register(Category)
@@ -8,6 +8,16 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 @admin.register(Budget)
-class CategoryExpense(admin.ModelAdmin):
+class BudgetAdmin(admin.ModelAdmin):
     list_display = ('category', 'amount',)
     search_fields = ('category',)
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'amount', 'date',)
+    search_fields = ('category', 'date')
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'amount', 'date',)
+    search_fields = ('name', 'date',)
--- expense_tracker/tracker_core/views.py
+++ expense_tracker/tracker_core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category
+from .forms import RegistrationForm
 
 # Create your views here.
 def index(request):
@@ -17,3 +18,18 @@ def expenses(request):
 def income(request):
     print("loading income")
     return render(request, 'income.html')
+
+def registration(request):
+    print("loading registration")
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = RegistrationForm
+    return render(request, 'registration.html', {'form': form})
+
+def profile(request):
+    '''Insert profile template and other information here once users login.'''
+    pass
