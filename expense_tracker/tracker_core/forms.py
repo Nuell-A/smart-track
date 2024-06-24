dbe5b49b4f2581b3fd829e323f1557808ec5a3c8
Nuell-LTL <alcala.arredondo1@gmail.com>
2024-06-23 21:51:46 -0700

    Added form for Category and implemented basic view for testing in views.py and HTML. Confirmed, data can now be submitted from the user.

--- expense_tracker/tracker_core/forms.py
+++ expense_tracker/tracker_core/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Category, Budget, Expense, Income
+
+'''Forms for models. Used to create entries into database.'''
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +11,9 @@ class RegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2',)
+
+class CategoryForm(forms.ModelForm):
+    
+    class Meta:
+        model = Category
+        fields = ('name',)
--- expense_tracker/tracker_core/views.py
+++ expense_tracker/tracker_core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Category
-from .forms import RegistrationForm
+from .forms import RegistrationForm, CategoryForm
 
 # Create your views here.
 def index(request):
@@ -8,9 +8,26 @@ def index(request):
     return render(request, 'index.html')
 
 def budget(request):
-    print("loading budget")
-    return render(request, 'budget.html')
-
+    '''Accepts post requests to add categories.
+    If form is submitted correctly, saves the entry into database with 
+    logged in user or with default user if no user is logged in.'''
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category = form.save(commit=False) # Does not save entry into database yet
+            if request.user.is_authenticated: # Checks if user is logged and sets user
+                category.user = request.user
+            else:
+                category.user_id = 1 # If no user is logged in, uses default user
+            
+            category.save()
+            return redirect('budget')
+        else:
+            print("there was an error with the form")
+    else:
+        form = CategoryForm
+    return render(request, 'budget.html', {'form': form})
+    
 def expenses(request):
     print("loading expenses")
     return render(request, 'expenses.html')
@@ -23,7 +40,7 @@ def registration(request):
     print("loading registration")
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
-        if form.is_valid():
+        if form.is_valid(): # Saves user if form is correct.
             form.save()
             return redirect('login')
     else:
